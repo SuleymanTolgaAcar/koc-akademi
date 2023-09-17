d28993d7b24573dc0b8c36b3a03bf02d4b34a54a
SuleymanTolgaAcar <tolgasuleymanacar@gmail.com>
2023-09-17 21:16:50 +0300

    changed main page margins for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import { IoIosArrowDropdown } from "react-icons/io";
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
-      <div className="home-bg w-full h-screen pt-20 pb-12 flex items-center justify-between flex-col text-blue-200 mb-16">
+      <div className="home-bg w-full h-screen pt-20 md:pb-12 pb-20 flex items-center justify-between flex-col text-blue-200 mb-16">
         <div></div>
         <div className="text-center">
           <h1 className="text-8xl md:text-9xl max-w-sm md:max-w-none text-center mb-4">
