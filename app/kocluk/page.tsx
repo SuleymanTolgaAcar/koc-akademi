9305206e18e2da73262af0b5c5f74aca92dafbce
Süleyman Acar <suleyman.acar@boun.edu.tr>
2023-09-24 21:46:04 +0300

    added mehmet ali

--- app/kocluk/page.tsx
+++ app/kocluk/page.tsx
@@ -41,6 +41,27 @@ const Kocluk = () => {
             full
           />
           <Card
+            name="Furkan Cengiz"
+            department="Boğaziçi Üniversitesi Bilgisayar Mühendisliği"
+            title="TYT 2.si, Sözel 8.si, Sayısal 309.su"
+            price={750}
+            rating={0}
+            description="2021 YKS'de sayısal alanda 309. EA alanında 2015.oldum ve 2021 yılında Boğaziçi Bilgisayar mühendisliğine yerleştim. 2023 YKS'ye ise sadece 1 hafta hazırlanıp Sözelde 8. TYT'de ise 2.oldum. 2 sene boyunca TYT adına bir soru bile çözmedim. Buna rağmen 2.olmayı başardım. Kendimde gözlemlediğim kritik noktaları koçluk öğrencilerime göstermek için sabırsızlanıyorum."
+            image="bg-furkancengiz"
+            docLink="https://drive.google.com/file/d/16Oil0wCSDS0-FlWCIZo-wWezbh_JBEvN/view?usp=drive_link"
+          />
+          <Card
+            name="Emre Şahin"
+            department="Hacettepe Üniversitesi Tıp"
+            title="Sayısal 830.su"
+            price={650}
+            rating={0}
+            description="Merhaba ben Emre.Hacettepe Üniversitesinde tıp 2. Sınıfım.2021 YKS sınavında sayısalda 830. Oldum. Üniversite sınavına hazırlanan sizlere yardımcı olmak için buradayım.Üniversite sınavına 
+            hazırlanma maratonunda karşılaşılabilecek sorunlar olduğunun farkındayım. Bu yüzden herkesin bir yol göstericiye ihtiyacı olabileceğini düşünüyorum.Bu yüzden buradayım. Yol göstermekte ve sınav maratonunuzu başarılı bir şekilde atlatmanız  için tecrübelerimi sizlere aktarmaktan mutluluk duyarım."
+            image="bg-emresahin"
+            docLink="https://drive.google.com/file/d/1VK4afPwCrIfHAiDFOj9RO9g0zqTQ4LQ2/view?usp=drive_link"
+          />
+          <Card
             name="N. Zülal İlhan"
             department="Boğaziçi Üniversitesi Ekonomi"
             title="Eşit Ağırlık 601.si"
@@ -54,7 +75,7 @@ const Kocluk = () => {
             name="Yunus Emre Korkmaz"
             department="Koç Üniversitesi Bilgisayar Mühendisliği"
             title="Sayısal 1126.sı"
-            price={500}
+            price={650}
             rating={0}
             description="Merhaba, ben Yunus. Koç Üniversitesinde bilgisayar mühendisliği okuyorum. Üniversite sınavına hazırlanan öğrencilere koçluk yapmak istiyorum. Neden mi? Çünkü ben de sizin gibi bir zamanlar üniversite hayalini kuran, çok çalışan ve başarılı olan bir öğrenciydim. Üniversite sınavında sayıdal alanda 1126. olarak Türkiye’nin en prestijli üniversitelerinden birine burslu olarak girdim. Bu başarımın arkasında yalnızca çalışkanlık değil, aynı zamanda doğru yöntemler, stratejiler ve motivasyon da vardı. Ben de size bu konularda yardımcı olmak, sizi desteklemek ve yol göstermek istiyorum.
   "
@@ -77,9 +98,19 @@ const Kocluk = () => {
             title="Sayısal 2881.su"
             price={500}
             rating={0}
-            description="Ben Mehmet Oğuz Kocadere, 20 yaşındayım. Hacettepe Bilgisayar Mühendisliği 2. Sınıf öğrencisiyim. YKS’ de sayısal 2881 sıralama yaparak bu okulu kazandım. Bunun için en çok bana katkı sağlayan alanlar AYT matematik, biyoloji ve kimya netlerim oldu ve kendime bu alanlarda güveniyorum. Bu yolda ilerlerken kullandığım yöntemleri anlatmaya ve aklınıza takılan sorularda yardım etmeye ve rehberiniz olmaya hazırım. "
+            description="Ben Mehmet Oğuz Kocadere, 20 yaşındayım. Hacettepe Bilgisayar Mühendisliği 2. Sınıf öğrencisiyim. YKS’ de sayısal 2881 sıralama yaparak bu okulu kazandım. Bunun için en çok bana katkı sağlayan alanlar AYT matematik, biyoloji ve kimya netlerim oldu ve kendime bu alanlarda güveniyorum. Bu yolda ilerlerken kullandığım yöntemleri anlatmaya ve aklınıza takılan sorularda yardım etmeye ve rehberiniz olmaya hazırım."
             image="bg-mehmetoguz"
             docLink="https://drive.google.com/file/d/1TbENzq6GMfhqHTEaCCcIZKLfufyeirBJ/view?usp=sharing"
+          />
+          <Card
+            name="Yusuf Yağcı"
+            department="Marmara Üniversitesi Tıp (İngilizce)"
+            title="Sayısal 3672.si"
+            price={500}
+            rating={0}
+            description="Merhaba, Ben Yusuf Yağcı. YKS 2021'de SAY 3672.siyim.Şu anda Marmara Üniversitesi Tıp (İngilizce) bölümünde okumaktayım.Bu sürecin içinden geçmiş birisi olarak sizlere nasıl düzenli çalışılır, nelere dikkat edilir, sınav zamanı stratejik konularda sizlere öncü olmak isterim. Birlikte derece yapmaya var mısınız?"
+            image="bg-yusufyagci"
+            docLink="https://drive.google.com/file/d/1URhSC2Epa61rP58ojhXqW58fdbMTWrvx/view?usp=drive_link"
           />
         </div>
       </div>
